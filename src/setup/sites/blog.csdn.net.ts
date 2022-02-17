9a03fc555e0854740fb35e6ccac44ddd4fec34b5
陆冬敏 <dongmin.lu@davyin.com>
2022-02-17 17:32:19 +0800

    优化提示

--- src/setup/sites/blog.csdn.net.ts
+++ src/setup/sites/blog.csdn.net.ts
@@ -1,12 +1,20 @@
-import { message } from 'antd'
+import { notification } from 'antd'
 import $ from 'jquery'
 
 $(() => {
   if (location.host !== 'blog.csdn.net') return
 
-  message.info('正在处理不能选择的代码...')
+  notification.info({
+    message: '正在处理不能选择的代码...',
+    description: '',
+  })
+
   $('#content_views pre code').css({
     'user-select': 'text',
   })
-  message.success('完成处理，所有的代码均可选择！')
+
+  notification.success({
+    message: '完成处理，所有的代码均可选择！',
+    description: '',
+  })
 })

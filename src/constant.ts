ac0630f4eb966065e25bd243d1cde879888d74f1
min牧曲 <411726355@qq.com>
2024-09-19 09:41:30 +0800

    fix

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,3 @@
-export const NAMESPACE = 'ludongmin-'
-export const DATE_FORMAT = 'YYYY-MM-DD'
-export const TIME_FORMAT = 'HH:mm:ss'
+export const NAMESPACE = "chrome-plugin-";
+export const DATE_FORMAT = "YYYY-MM-DD";
+export const TIME_FORMAT = "HH:mm:ss";

ca80d39f3ad87132002c55173d7c44c67209cd52
min牧曲 <411726355@qq.com>
2024-09-19 10:05:09 +0800

    升级api

--- src/setup/handle/injectJs.ts
+++ src/setup/handle/injectJs.ts
@@ -1,6 +1,6 @@
 export default (jsPath: string) => {
-  const temp = document.createElement('script')
-  temp.setAttribute('type', 'text/javascript')
-  temp.src = chrome.extension.getURL(jsPath)
-  document.body.appendChild(temp)
-}
+  const temp = document.createElement("script");
+  temp.setAttribute("type", "text/javascript");
+  temp.src = chrome.runtime.getURL(jsPath);
+  document.body.appendChild(temp);
+};
